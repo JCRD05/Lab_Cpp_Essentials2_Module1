19afff0ac839be710dc8cd5b1c51e14283b492aa
JCRD05 <josecarlosruizdiaz05@gmail.com>
2025-08-29 09:16:34 -0600

    Lab 107

--- Lab_1.07/Lab_107.cpp
+++ Lab_1.07/Lab_107.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <string>
+
+class Person
+{
+public:
+	std::string name;
+	int age;
+	int weight;
+	float height;
+};
+
+void print(Person* person)
+{
+	std::cout << person->name << " is " << person->age << " years old, he weights " 
+		<< person->weight << " kg and is "  << person->height << " meters tall\n\n";
+}
+
 
 int main()
 {
-	std::cout << "Hello World!";
+	Person person1;
+	person1.name = "Jose Carlos";
+	person1.age = 20;
+	person1.weight = 90;
+	person1.height = 1.80;
+
+	std::cout << "Meet " << person1.name << "\n";
+	print(&person1);
+
+	Person person2;
+	person2.name = "Valerie";
+	person2.age = 19;
+	person2.weight = 60;
+	person2.height = 1.66;
+
+	std::cout << "Meet " << person2.name << "\n";
+	print(&person2);
+
+	Person person3;
+	person3.name = "Anton";
+	person3.age = 20;
+	person3.weight = 81;
+	person3.height = 1.65;
+
+	std::cout << "Meet " << person3.name << "\n";
+	print(&person3);
+
+	return 0;
 }
--- Lab_1.08/Lab_108.cpp
+++ Lab_1.08/Lab_108.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <string>
+
+class Square
+{
+public:
+	Square(double side);
+	double    side;
+	double    area;
+	// Your code here
+};
+
+Square::Square(double side)
+{
+	this->side = side;
+	this->area = side * side;
+}
+
+void print(Square* square)
+{
+	std::cout << "Square: side = " << square->side << " area = " << square->area << "\n\n";
+}
+
 
 int main()
 {
-	std::cout << "Hello World!";
+	Square s(4);
+
+
+	print(&s);
+
+	s.side = 2.0;
+	print(&s);
+
+	s.side = -33.0;
+	print(&s);
+
+	return 0;
 }
